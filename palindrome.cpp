9f0e07d52caaed673fad4a8c3aaa0fa5a79b377b
Alex Milanez <amilanez@horizon.csueastbay.edu>
2022-10-04 10:47:24 -0700

    Files added and Palindrome file

---
+++ palindrome.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Alexandro Milanez on 10/4/2022.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string s) {
+    if(s.length()<=1){
+        return true;
+    }
+    else {
+        string tmp = s.substr(1, s.length()-2);
+        if(s[0]==s[s.length()-1]){
+            return isPalindrome(tmp);
+        }
+        else{
+            return false;
+        }
+    }
+}
+int main(){
+    string str;
+    string s;
+    cout<<"Enter a String to test: ";
+    getline(cin, str);
+    cin>>s;
+    if(isPalindrome(s)){
+        cout<<"The string '"<<s<<"' is a palindrome."<<endl;
+    }
+    else{
+        cout<<"The string '"<<s<<"' is not a palindrome."<<endl;
+    }
+    return 0;
+}
